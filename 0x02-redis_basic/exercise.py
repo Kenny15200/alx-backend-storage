d7380eb824b43de9086e77fe538f8b905302a05d
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-21 10:28:05 -0500

    Storing listRetrieving listss

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -46,6 +46,23 @@ def call_history(method: Callable) -> Callable:
         return result
     return wrapper
 
+def replay(method: Callable) -> None:
+    """
+    Display the history of calls of a particular function.
+    
+    Args:
+        method: The method to display the call history for.
+    """
+    cache = method.__self__
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+    inputs = cache._redis.lrange(input_key, 0, -1)
+    outputs = cache._redis.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input, output in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{input.decode('utf-8')}) -> {output.decode('utf-8')}")
+
 class Cache:
     """Cache class to interact with Redis."""
     def __init__(self):
